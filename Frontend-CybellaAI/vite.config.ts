214ce2290bf8361522abab9f83d7533e956af045
hartonoosu <hartonoosu@gmail.com>
2025-06-01 21:40:30 +1000

    fix: sync router basename with /Frontend-CybellaAI path

--- Frontend-CybellaAI/vite.config.ts
+++ Frontend-CybellaAI/vite.config.ts
@@ -6,7 +6,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   // base:"/",
-  base:"/Test-CybellaAI/",
+  base:"/Frontend-CybellaAI/",
   build:{
     outDir: "dist",
   },
